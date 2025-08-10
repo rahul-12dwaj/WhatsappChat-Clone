9b625d44d528e3e4d8ea9990b5039561bf497a7a
rahul-12dwaj <rahulbhardwaj4239@gmail.com>
2025-08-10 21:08:23 +0530

    Backend fixed 5

--- my-backend/server.js
+++ my-backend/server.js
@@ -88,7 +88,7 @@ mongoose
 
     // Start the server after seeding
     const port = process.env.PORT || 5000;
-    app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${port}`));
   })
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
